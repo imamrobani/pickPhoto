53a7f385b447842d0af20311600a2c430363da57
Imam Robani <imamrob@gmail.com>
2021-02-06 08:40:51 +0700

    add const Fonts

---
+++ src/const/Fonts.js
@@ -0,0 +1,10 @@
+const Fonts = {
+  POPPINS_LIGHT: 'Poppins-Light',
+  POPPINS_MEDIUM: 'Poppins-Medium',
+  POPPINS_REGULAR: 'Poppins-Regular',
+  POPPINS_ITALIC: 'Poppins-Light',
+  POPPINS_BOLD: 'Poppins-Bold',
+  POPPINS_THIN: 'Poppins-Thin'
+}
+
+export default Fonts
