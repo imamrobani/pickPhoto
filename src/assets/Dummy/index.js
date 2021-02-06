15abad39276ef2f4863a33562615a744b3ae8a33
Imam Robani <imamrob@gmail.com>
2021-02-06 12:55:58 +0700

    add dummy photo

--- src/assets/Dummy/index.js
+++ src/assets/Dummy/index.js
@@ -1,5 +1,15 @@
 import ProfileDummy from './profile-dummy.png'
+import Dummy1 from './dummy1.png'
+import Dummy2 from './dummy2.png'
+import Dummy3 from './dummy3.png'
+import Dummy4 from './dummy4.png'
+import Dummy5 from './Dummy5.jpg'
 
 export {
   ProfileDummy,
+  Dummy1,
+  Dummy2,
+  Dummy3,
+  Dummy4,
+  Dummy5
 }
